eef112ef0eb643cdfc7ec917112aa1736978e4eb
sonesoul <smippy098@gmail.com>
2025-02-08 02:32:50 +0200

    Moved Time class to a separate file

--- Frame.cs
+++ Frame.cs
@@ -37,24 +37,4 @@ namespace PixelBox
         }
         public static void HandlePostDraw(GameTime gameTime) => Time.Update(gameTime);
     }
-    public static class Time
-    {
-        public static float Delta { get; private set; }
-        public static float RealDelta { get; private set; }
-        public static float FixedDelta { get; set; } = 1.0f / 120;
-
-        public static GameTime GameTime { get; private set; }
-       
-        public static float TimeScale { get => _timeScale; set => _timeScale = value.ClampMin(0); }
-
-        private static float _timeScale = 1f;
-
-        public static void Update(GameTime gameTime)
-        {
-            GameTime = gameTime;
-            
-            RealDelta = (float)gameTime.ElapsedGameTime.TotalSeconds;
-            Delta = RealDelta * TimeScale;
-        }
-    }
 }
---
+++ Time.cs
@@ -0,0 +1,23 @@
+﻿namespace PixelBox
+{
+    public static class Time
+    {
+        public static float Delta { get; private set; }
+        public static float RealDelta { get; private set; }
+        public static float FixedDelta { get; set; } = 1.0f / 120;
+
+        public static GameTime GameTime { get; private set; }
+
+        public static float TimeScale { get => _timeScale; set => _timeScale = value.ClampMin(0); }
+
+        private static float _timeScale = 1f;
+
+        public static void Update(GameTime gameTime)
+        {
+            GameTime = gameTime;
+
+            RealDelta = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            Delta = RealDelta * TimeScale;
+        }
+    }
+}
