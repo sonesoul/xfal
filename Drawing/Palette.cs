75b16d3f821cda3a184a3290cb2fd531e2308ba3
sonesoul <smippy098@gmail.com>
2025-02-26 13:36:03 +0200

    added absolute white color to support one bit games

--- Drawing/Palette.cs
+++ Drawing/Palette.cs
@@ -1,48 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace PixelBox.Drawing
 {
     public static class Palette
     {
-        public static readonly int Black = 0;
-        public static readonly int DarkBlue = 1;
-        public static readonly int DarkPurple = 2;
-        public static readonly int DarkGreen = 3;
-        public static readonly int Brown = 4;
-        public static readonly int DarkGray = 5;
-        public static readonly int LightGray = 6;
-        public static readonly int White = 7;
-        public static readonly int Red = 8;
-        public static readonly int Orange = 9;
-        public static readonly int Yellow = 10;
-        public static readonly int Green = 11;
-        public static readonly int Blue = 12;
-        public static readonly int Indigo = 13;
-        public static readonly int Pink = 14;
-        public static readonly int Peach = 15;
-        public static readonly int Transparent = 16;
+        public const int Black = 0;
+        public const int White = 1;
+        public const int DarkBlue = 2;
+        public const int DarkPurple = 3;
+        public const int DarkGreen = 4;
+        public const int Brown = 5;
+        public const int DarkGray = 6;
+        public const int LightGray = 7;
+        public const int SoftWhite = 8;
+        public const int Red = 9;
+        public const int Orange = 10;
+        public const int Yellow = 11;
+        public const int Green = 12;
+        public const int Blue = 13;
+        public const int Indigo = 14;
+        public const int Pink = 15;
+        public const int Peach = 16;
+        public const int Transparent = 17;
 
         private static readonly Color[] Colors = new[]
         {
             new Color(0, 0, 0),         // 0 Black
-            new Color(29, 43, 83),      // 1 Dark Blue
-            new Color(126, 37, 83),     // 2 Dark Purple
-            new Color(0, 135, 81),      // 3 Dark Green
-            new Color(171, 82, 54),     // 4 Brown
-            new Color(95, 87, 79),      // 5 Dark Gray
-            new Color(194, 195, 199),   // 6 Light Gray
-            new Color(255, 241, 232),   // 7 White
-            new Color(255, 0, 77),      // 8 Red
-            new Color(255, 163, 0),     // 9 Orange
-            new Color(255, 236, 39),    // 10 Yellow
-            new Color(0, 228, 54),      // 11 Green
-            new Color(41, 173, 255),    // 12 Blue
-            new Color(131, 118, 156),   // 13 Indigo
-            new Color(255, 119, 168),   // 14 Pink
-            new Color(255, 204, 170),   // 15 Peach
-            new Color(0, 0, 0, 0)       // 16 Transparent
+            new Color(255, 255, 255),   // 1 White
+            new Color(29, 43, 83),      // 2 Dark Blue
+            new Color(126, 37, 83),     // 3 Dark Purple
+            new Color(0, 135, 81),      // 4 Dark Green
+            new Color(171, 82, 54),     // 5 Brown
+            new Color(95, 87, 79),      // 6 Dark Gray
+            new Color(194, 195, 199),   // 7 Light Gray
+            new Color(255, 241, 232),   // 8 White
+            new Color(255, 0, 77),      // 9 Red
+            new Color(255, 163, 0),     // 10 Orange
+            new Color(255, 236, 39),    // 11 Yellow
+            new Color(0, 228, 54),      // 12 Green
+            new Color(41, 173, 255),    // 13 Blue
+            new Color(131, 118, 156),   // 14 Indigo
+            new Color(255, 119, 168),   // 15 Pink
+            new Color(255, 204, 170),   // 16 Peach
+            new Color(0, 0, 0, 0)       // 17 Transparent
         };
         
         public static Color GetColor(int index) => Colors[index];
