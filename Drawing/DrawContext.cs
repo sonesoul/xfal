d7ba3106de6aaa8ce42992ec92d8fed6e150bf91
sonesoul <smippy098@gmail.com>
2026-02-24 00:09:25 -0700

    move DrawOptions to a separate file

--- Drawing/DrawContext.cs
+++ Drawing/DrawContext.cs
@@ -141,18 +141,4 @@ namespace xfal.Drawing
             Polygon(vertices, color, boundThickness);
         }
     }
-    public struct DrawOptions
-    {
-        public Vector2 position = Vector2.Zero;
-        public Vector2 origin = Vector2.Zero;
-        public Vector2 scale = Vector2.One;
-
-        public Color color = Color.White;
-        public float rotationRad = 0;
-       
-        public DrawOptions()
-        {
-
-        }
-    }
 }
---
+++ Drawing/DrawOptions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace xfal.Drawing
+{
+    public struct DrawOptions
+    {
+        public Vector2 position = Vector2.Zero;
+        public Vector2 origin = Vector2.Zero;
+        public Vector2 scale = Vector2.One;
+
+        public Color color = Color.White;
+        public float rotationRad = 0;
+
+        public DrawOptions() { }
+    }
+}
